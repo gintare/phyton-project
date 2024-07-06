519c2d6f515704f98f4dd368d4cc02eba2a8cb26
gintare <gintare.ragaisiene@gmail.com>
2024-07-06 20:21:37 +0300

    Remove commented lines

--- measurements_test.py
+++ measurements_test.py
@@ -3,26 +3,6 @@ import measurements
 from datetime import datetime
 
 
-# def parseLine(line: str) -> measurements.Measurement:
-#     stripped_string = line.strip("{").strip()
-#     stripped_string1 = stripped_string[:-1]
-#     parts = stripped_string1.strip().split(',')
-#     date = datetime.strptime(parts[0], "%Y-%m-%dT%H:%M:%S")
-#     measType = measurements.MeasType.from_str(parts[1].strip())
-#     value = float(parts[2].strip())
-#     return measurements.Measurement(measurementTime=date, measurementType=measType, value=value)
-#
-#
-# def readFileToDataclass(filename: str) -> list[measurements.Measurement]:
-#     records = []
-#     with open(filename, 'r') as file:
-#         for line in file:
-#             parseLine(line)
-#             record = parseLine(line)
-#             records.append(record)
-#     return records
-
-
 def mapDictResultToList(result):
     resultToList = []
     for key, value in result.items():
